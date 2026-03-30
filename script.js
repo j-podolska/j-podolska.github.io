0459e36816fb39aeb82f1cc4966360b5520fc68d
j-podolska <julia.podolska@student.uj.edu.pl>
2026-03-30 12:08:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+body {
+  margin: 0;
+  font-family: 'Lustria', serif;
+  background-color: #cfe8ff;
+  color: #6b4f3b;
+}
+
+body.dark {
+  background-color: #6b4f3b;
+  color: #ffffff;
+}
+
+header {
+  position: relative;
+  text-align: center;
+  padding: 2rem;
+}
+
+header h1 {
+  font-family: 'Brawler', serif;
+}
+
+nav {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+}
+
+nav button {
+  margin-left: 0.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  cursor: pointer;
+  background: #ffffff;
+  color: #6b4f3b;
+}
+
+.section {
+  max-width: 800px;
+  margin: 2rem auto;
+  padding: 1rem;
+  text-align: left;
+}
+
+.section h2 {
+  font-family: 'Brawler', serif;
+  margin-bottom: 1rem;
+}
+
+.section ul {
+  padding-left: 1.2rem;
+}
+
+footer {
+  text-align: center;
+  padding: 1rem;
+  background-color: #ffffff;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
